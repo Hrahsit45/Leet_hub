5697335bfd2afaf9cfe8736d007857952c97e320
Hrahsit45 <59120087+Hrahsit45@users.noreply.github.com>
2022-08-17 01:37:32 +0530

    Time: 234 ms (77.70%), Space: 75.2 MB (86.54%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -5,27 +5,32 @@ public:
     
     int maxProfit(vector<int>& a) {
         
-   
+    vector<vector<int>> dp(2 , vector<int> (2 , 0));
     
-      int buy1 = INT_MIN;
-      int sell = 0;
-        
-     int buy2 = INT_MIN;
-     int sell2 = 0;
+    dp[0][0] = INT_MIN;
+    dp[1][0] = 0;
+    dp[0][1] = INT_MIN;
         
         for(int i = 0 ; i < a.size() ; i++)
         {
             
+            //1.buy
+            dp[0][0] = max(dp[0][0] , -a[i]);
+                
+            //2.sell
+            dp[1][0] = max(dp[1][0] , dp[0][0]+a[i]);
+  
+            //when one chance is left 
             
-          buy1 = max(buy1 , -a[i]);
-          sell = max(sell , buy1 + a[i]);
+            //1.buy
+            dp[0][1] = max(dp[1][0]-a[i] , dp[0][1]);
             
-          buy2 = max(buy2 , sell-a[i]);
-          sell2 = max(sell2 , buy2+a[i]);  
-           
+            //2.sell
+            dp[1][1] = max(dp[0][1] + a[i] , dp[1][1]);
+            
               
         }
         
-        return sell2;
+        return dp[1][1];
     }
 };
