03f6a8021d25124203d7ac74af6616d09753b409
Hrahsit45 <59120087+Hrahsit45@users.noreply.github.com>
2022-09-14 15:30:41 +0530

    Time: 2 ms (44.11%), Space: 6.1 MB (11.44%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -4,9 +4,23 @@ public:
     
     int numTrees(int n) {
         
-        memo.resize(n+1 , 0);
+         vector< long long int> catalan(n+1);
         
-        return count(n);
+        catalan[0] = catalan[1] = 1;
+        
+        for(int i = 2 ; i<= n ; i++)
+        {
+            //catalan[i] = 0;
+            for(int j = 0 ; j<i ; j++)
+            {
+                catalan[i] += catalan[j]*catalan[i - j - 1];
+            }
+        }
+        return catalan[n];  
+        
+//         memo.resize(n+1 , 0);
+        
+//         return count(n);
     }
     
     int count(int n)
