4944f54ef20f6bc6b61d536f37afd69bdb4512e7
Hrahsit45 <59120087+Hrahsit45@users.noreply.github.com>
2022-09-20 07:41:26 +0530

    Time: 20 ms (51.60%), Space: 10.9 MB (54.19%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -8,11 +8,13 @@ public:
         
         for(int i = 1 ; i <= s.size(); i++)
         {
+            string word = "";
             for(int j = i - 1 ;  j>= 0 ; j-- )
             {
+                word = s[j]+word;
                 if(dp[j])
                 {
-                    string word = s.substr(j , i - j);
+                    
                     
                     if(find(wordDict.begin() , wordDict.end() , word) != wordDict.end())
                     {
