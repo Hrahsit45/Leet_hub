5335a4d357ae7cf82d979a4a7ce79d0d98e6e2a5
Hrahsit45 <59120087+Hrahsit45@users.noreply.github.com>
2022-10-02 16:49:47 +0530

    Time: 564 ms (19.75%), Space: 48.3 MB (77.34%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+       
+       
+        int n = nums.size() ;
+        if(n == 1)
+            return true;
+        
+        int steps  = 0 ;
+        
+        for(int i = 0 ; i < n ; i++)
+        {
+            steps = max(steps , nums[i]);
+            steps--;
+            
+            cout<<steps<<"\n";
+            
+            if(steps < 0 && i != n-1)
+                return false;
+        }
+        
+        return true;
+    }
+};
